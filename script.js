4134afdf0c12af409e015e9e04a94ffda529a255
daedalus <j.jozefczyk90@gmail.com>
2022-11-07 22:40:02 +0000

    finished css

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//    Setting Game board
+const gameBoard = (() => {
+  const board = [];
+  return {};
+})();
+
+const player = (symbol) => {
+  const playerX = 'X';
+  const PlayerO = 'O';
+};
